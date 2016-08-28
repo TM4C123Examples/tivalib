962efda84d14ff0812f2dbb54338f42d2129efa7
Manuel Limones <limonesu.me@gmail.com>
2016-08-28 03:13:54 -0500

    format code

--- examples/ex_02_button/source/main.c
+++ examples/ex_02_button/source/main.c
@@ -1,14 +1,14 @@
 #include "board_tm4c123gxl.h"
 #include "delay.h"
 int main(){
-	led_init();
+    led_init();
     buttons_init();
-	while(1){
+    while(1){
     if (button_get_sw1(1))
         led_set_color(LED_GREEN);
     else
         led_set_color(LED_BLACK);
-    
+
     if (button_get_sw2(1))
         led_set_color(LED_BLUE);
     else
--- tivalib/source/board_tm4c123gxl.c
+++ tivalib/source/board_tm4c123gxl.c
@@ -7,29 +7,29 @@ void led_init(void){
     GPIOF->DIR|=(0x1<<1)|(0x1<<2)|(0x1<<3);//Activamos pin F1 F2 F3
 }
 void buttons_init(void){
-	SYSCTL->RCGCGPIO|=(0x1<<5);//Activamos Periferico GPIOF
+    SYSCTL->RCGCGPIO|=(0x1<<5);//Activamos Periferico GPIOF
     GPIOF->LOCK=(0x4C4F434B);
     GPIOF->CR=(0x1<<0);
-    
-	GPIOF->DEN|=(0x1<<0)|(0x1<<4);
-	GPIOF->PUR|=(0x1<<0)|(0x1<<4);
+
+    GPIOF->DEN|=(0x1<<0)|(0x1<<4);
+    GPIOF->PUR|=(0x1<<0)|(0x1<<4);
 }
 void led_set_color(int color){
     ((uint32_t*)GPIOF)[LED_WHITE]=color;
 }
 int  button_get_sw1(int state){
-    if (state == 1){ 
+    if (state == 1){
         if (((GPIOF->DATA&(0x1<<4))))
             return 0;
-        else 
+        else
             return 1;}
 }
 
 int  button_get_sw2(int state){
-    if (state == 1){ 
+    if (state == 1){
         if (((GPIOF->DATA&(0x1<<0))))
             return 0;
-        else 
+        else
             return 1;}
-        
+
 }
