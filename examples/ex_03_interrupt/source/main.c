da08259580f3d48b1d30a89b2c059f160a445383
oscar <enriquez.oscar@outlook.com>
2016-08-28 04:11:19 -0500

    add interupt to buttons

--- examples/ex_03_interrupt/source/main.c
+++ examples/ex_03_interrupt/source/main.c
@@ -1,36 +1,42 @@
 #include "board_tm4c123gxl.h"
 #include "delay.h"
 #include "TM4C123.h"                    // Device header
-
-void configPeripherial(void);
+void buttons_interupt_init(void);
 
 int main(){
-  configPeripherial();
-  NVIC_EnableIRQ(GPIOF_IRQn); 	
+    buttons_interupt_init();
+    led_init();
+    NVIC_EnableIRQ(GPIOF_IRQn);
 	while(1) ;
-	//return 0;
+
 }
 
 
-//input is SW1==PF4 use pull ups
-void configPeripherial(void){
+//input is SW1==PF4 SW"== PF0 use pull ups
+void buttons_interupt_init(void){
 	SYSCTL->RCGCGPIO|=((0x1)<<5);//enable GPIOF peripherial
-	GPIOF->DEN|=0x1F;//enable GPIOF[4:0] as digital IO
-	GPIOF->DIR|=(0x1<<1)|(0x1<<2)|(0x1<<3);// GPIOF[3:1] as output
-	GPIOF->PUR=(0x1<<4);//GPIOF[4] is using PullUp resitors
-	
-	GPIOF->IBE|=(0x1<<4);//both edeges interrupt sensing
-	GPIOF->IM=(0x1<<4);//Disable the interrupt mask for GPIOF[4]
-	GPIOF->ICR=(0x1<<4);//clear previous interrupts events for GPIOF[4]
+    GPIOF->LOCK=(0x4C4F434B); // eneble sw2
+    GPIOF->CR=(0x1<<0);// sure eneble sw2
+	GPIOF->DEN|=(0x1<<4)|(0x1<<0);//enable GPIOF[4&0] as digital IO
+	GPIOF->PUR=(0x1<<4)|(0x1<<0);//GPIOF[4] and GPIOF[0]is using PullUp resitors
+	GPIOF->IBE|=(0x1<<4)|(0x1<<0);//both edeges interrupt sensing
+	GPIOF->IM=(0x1<<4)|(0x1<<0);//Disable the interrupt mask for GPIOF[4]
+	GPIOF->ICR=(0x1<<4)|(0x1<<0);//clear previous interrupts events for GPIOF[4]
 }
 
 void GPIOF_Handler(void){
-	GPIOF->ICR=(0x1<<4);//clear previous interrupts events for GPIOF[4]
+	GPIOF->ICR=(0x1<<4)||(0x1<<0);//clear previous interrupts events for GPIOF[4&0]
 	//User code
 	if(((GPIOF->DATA>>4)&0x1)==0){
-		GPIOF->DATA=0xF;
+		led_set_color(LED_GREEN);
 	}
-  else{
-		GPIOF->DATA=0x0;
+    else{
+        led_set_color(LED_BLACK);
+	}
+    if(((GPIOF->DATA>>0)&0x1)==0){
+		led_set_color(LED_BLUE);
+	}
+    else{
+      led_set_color(LED_BLACK);
 	}
 }
