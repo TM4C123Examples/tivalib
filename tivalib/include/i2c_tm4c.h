293879e75b1fd45894d6f1fb5ef7ad14c2b9f696
Manuel Limones <limonesu.me@gmail.com>
2016-09-02 22:51:09 -0500

    Correct i2c_tm4c header file

--- tivalib/include/i2c_tm4c.h
+++ tivalib/include/i2c_tm4c.h
@@ -10,8 +10,7 @@ void i2c_sendData(unsigned char  address_7b,unsigned char data);
 unsigned char i2c_readData(unsigned char address_7b,unsigned char regAddress);
 void i2c_sendPacket(unsigned char  address_7b,unsigned char  *pData,int nData);
 int i2c_isBusy(void);
-void dac_output(short val);
-void dac_config(void);
+void i2c_readPacket(unsigned char  address_7b,unsigned char regAddress);
 
 #ifdef __cplusplus
 }
