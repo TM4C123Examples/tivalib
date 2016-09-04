10b3d3a19bbbda50497d7d96ac6bb1e969205434
oscar <enriquez.oscar@outlook.com>
2016-09-03 19:30:06 -0500

    try to fix pwm for low frequence

--- tivalib/source/timer_pwm_tm4c.c
+++ tivalib/source/timer_pwm_tm4c.c
@@ -1,17 +1,20 @@
 #include "TM4C123.h" // Device header
 #include "timer_pwm_tm4c.h"
+int frequence = 0;
+ 
 
-void timer_pwm_set(float duty_cycle){
+void timer0_pwm_set(float duty_cycle){
 	if(duty_cycle<=0){
-		TIMER0->TAMATCHR=(50000-1); //ciclo de trabajo 0
+		TIMER0->TAMATCHR=(frequence); //ciclo de trabajo 0
 	}else if(duty_cycle>=100){
 		TIMER0->TAMATCHR=0xFFFF; //ciclo de trabajo 100
 	}else if(duty_cycle>0&&duty_cycle<100){
-		TIMER0->TAMATCHR=(50000-1)-((int)((duty_cycle/100)*(50000)));
+		TIMER0->TAMATCHR=(frequence)-((int)((duty_cycle/100)*(frequence)));
 	}
 }
 
-int timer0_pwm_init(void){
+int timer0_pwm_init(int frequency_timer){
+    SystemCoreClockUpdate();
 	SYSCTL->RCGCGPIO|=0x3F;//Habilitamos todos los GPIO
 	GPIOF->DEN|=0x1<<1;
 	GPIOF->DIR|=0x1<<1;
@@ -37,12 +40,12 @@ int timer0_pwm_init(void){
 	TIMER0->TAMR&=~(0x1<<4);//counter down(Necesario para PWM)
 
 	//Valores de control de periodo de timer  periodo en ciclos =(TAPR<<16)|TAILR
-	TIMER0->TAILR=(50000)-1; //FREQ = 1kHz  REG = (sysClockFreq/pwmFreq)-1
+    frequence =(SystemCoreClock/frequency_timer);
+	TIMER0->TAILR=frequence - 1; //FREQ= (sysClockFreq/pwmFreq)-1
 	TIMER0->TAPR=0x00;
-
+ 
 	//Valores de control de ciclo de trabajo
-	TIMER0->TAMATCHR=50000-1; //ciclo de trabajo 0
+	TIMER0->TAMATCHR=frequence - 1; //ciclo de trabajo 0, 
 	TIMER0->TAPMR=0x00;
-
 	TIMER0->CTL|=0x1<<0;//inicia PWM
 }
