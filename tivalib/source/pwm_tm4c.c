b96c4df5505b8da249fabb7a45719840b199e3dc
Manuel Limones <limonesu.me@gmail.com>
2016-10-15 18:22:38 -0500

    Add parametrized pin configuration

--- tivalib/source/pwm_tm4c.c
+++ tivalib/source/pwm_tm4c.c
@@ -1,28 +1,38 @@
 #include "TM4C123.h"                    // Device header
 #include "pwm_tm4c.h"
 
+void gpio_set_af(GPIOA_Type *gpio,uint32_t pin,uint32_t af_select);
+void gpio_peripheral_enable(GPIOA_Type *gpio);
+int gpio_get_peripheral_index(GPIOA_Type *gpio);
+
+void gpio_set_af(GPIOA_Type *gpio,uint32_t pin,uint32_t af_select){
+        gpio->DEN |= (0x1<<pin);//digital functions
+        gpio->AFSEL |= (0x1<<pin);//alternate functions
+        gpio->PCTL &= ~(((uint32_t)0xF)<<(pin*4));
+        gpio->PCTL |= (((uint32_t)af_select)<<(pin*4));//Connect Pin6 to alternate function 4 (m0pwm0)
+}
+
+void gpio_peripheral_enable(GPIOA_Type *gpio){
+    int p_index = gpio_get_peripheral_index(gpio);
+    SYSCTL->RCGCGPIO |= (0x1<<p_index);
+    while(!(SYSCTL->PRGPIO & (0x1<<p_index)));
+}
+
+int gpio_get_peripheral_index(GPIOA_Type *gpio){
+    uint32_t address = (uint32_t) gpio;
+    return (address-GPIOA_BASE)/(GPIOB_BASE-GPIOA_BASE);
+}
+
 void pwm0_AB_init(float frequency,int mode,float pwm_a_initial_dc,float pwm_b_initial_dc){
     //Configure PB6 as alternate function for M0PWM0
     if((mode == PWM_A_ONLY)||(mode == PWM_A_AND_B)){
-        SYSCTL->RCGCGPIO |= (0x1<<1);//Turn on GPIOB
-        while(!(SYSCTL->PRGPIO & (0x1<<1)));
-
-        GPIOB->DEN |= (0x1<<6);//digital functions
-        GPIOB->AFSEL |= (0x1<<6);//alternate functions
-
-        GPIOB->PCTL &= ~(((uint32_t)0xF)<<(24));
-        GPIOB->PCTL |= (((uint32_t)0x4)<<(24));//Connect Pin6 to alternate function 4 (m0pwm0)
+        gpio_peripheral_enable(GPIOB);
+        gpio_set_af(GPIOB,6,4);//Connect Pin6 to alternate function 4 (m0pwm0)
     }
     //Configure PB7 as alternate function for M0PWM1
     if((mode == PWM_B_ONLY)||(mode == PWM_A_AND_B)){
-        SYSCTL->RCGCGPIO |= (0x1<<1);//Turn on GPIOB
-        while(!(SYSCTL->PRGPIO & (0x1<<1)));
-
-        GPIOB->DEN |= (0x1<<7);//digital functions
-        GPIOB->AFSEL |= (0x1<<7);//alternate functions
-
-        GPIOB->PCTL &= ~(((uint32_t)0xF)<<(28));
-        GPIOB->PCTL |= (((uint32_t)0x4)<<(28));//Connect Pin7 to alternate function 4 (m0pwm1)
+        gpio_peripheral_enable(GPIOB);
+        gpio_set_af(GPIOB,7,4);//Connect Pin7 to alternate function 4 (m0pwm1)
     }
 
     SYSCTL->RCC |= (0x1<<20);// Use pwm div
