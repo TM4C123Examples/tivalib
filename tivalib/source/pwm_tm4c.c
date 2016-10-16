ec2eb8e09d2a47af78dfdb1f99c46cdbf942efa0
Manuel Limones <limonesu.me@gmail.com>
2016-10-15 19:23:55 -0500

    Parametrize channel configuration

--- tivalib/source/pwm_tm4c.c
+++ tivalib/source/pwm_tm4c.c
@@ -23,6 +23,79 @@ int gpio_get_peripheral_index(GPIOA_Type *gpio){
     return (address-GPIOA_BASE)/(GPIOB_BASE-GPIOA_BASE);
 }
 
+typedef struct {
+    uint32_t  CTL;                            /*!< PWMX Control                                                          */
+    uint32_t  INTEN;                          /*!< PWMX Interrupt and Trigger Enable                                     */
+    uint32_t  RIS;                            /*!< PWMX Raw Interrupt Status                                             */
+    uint32_t  ISC;                            /*!< PWMX Interrupt Status and Clear                                       */
+    uint32_t  LOAD;                           /*!< PWMX Load                                                             */
+    uint32_t  COUNT;                          /*!< PWMX Counter                                                          */
+    uint32_t  CMPA;                           /*!< PWMX Compare A                                                        */
+    uint32_t  CMPB;                           /*!< PWMX Compare B                                                        */
+    uint32_t  GENA;                           /*!< PWMX Generator A Control                                              */
+    uint32_t  GENB;                           /*!< PWMX Generator B Control                                              */
+    uint32_t  DBCTL;                          /*!< PWMX Dead-Band Control                                                */
+    uint32_t  DBRISE;                         /*!< PWMX Dead-Band Rising-Edge Delay                                      */
+    uint32_t  DBFALL;                         /*!< PWMX Dead-Band Falling-Edge-Delay                                     */
+    uint32_t  FLTSRC0;                        /*!< PWMX Fault Source 0                                                   */
+    uint32_t  FLTSRC1;                        /*!< PWMX Fault Source 1                                                   */
+    uint32_t  MINFLTPER;                      /*!< PWMX Minimum Fault Period                                             */
+} PWM_GENERATOR_Type;
+
+typedef struct {                                    /*!< PWM0 Structure                                                        */
+  __IO uint32_t  CTL;                               /*!< PWM Master Control                                                    */
+  __IO uint32_t  SYNC;                              /*!< PWM Time Base Sync                                                    */
+  __IO uint32_t  ENABLE;                            /*!< PWM Output Enable                                                     */
+  __IO uint32_t  INVERT;                            /*!< PWM Output Inversion                                                  */
+  __IO uint32_t  FAULT;                             /*!< PWM Output Fault                                                      */
+  __IO uint32_t  INTEN;                             /*!< PWM Interrupt Enable                                                  */
+  __IO uint32_t  RIS;                               /*!< PWM Raw Interrupt Status                                              */
+  __IO uint32_t  ISC;                               /*!< PWM Interrupt Status and Clear                                        */
+  __IO uint32_t  STATUS;                            /*!< PWM Status                                                            */
+  __IO uint32_t  FAULTVAL;                          /*!< PWM Fault Condition Value                                             */
+  __IO uint32_t  ENUPD;                             /*!< PWM Enable Update                                                     */
+  __I  uint32_t  RESERVED[5];
+  __IO PWM_GENERATOR_Type  generator[4];
+  __I  uint32_t  RESERVED1[432];
+  __IO uint32_t  _0_FLTSEN;                         /*!< PWM0 Fault Pin Logic Sense                                            */
+  __I  uint32_t  _0_FLTSTAT0;                       /*!< PWM0 Fault Status 0                                                   */
+  __I  uint32_t  _0_FLTSTAT1;                       /*!< PWM0 Fault Status 1                                                   */
+  __I  uint32_t  RESERVED2[29];
+  __IO uint32_t  _1_FLTSEN;                         /*!< PWM1 Fault Pin Logic Sense                                            */
+  __I  uint32_t  _1_FLTSTAT0;                       /*!< PWM1 Fault Status 0                                                   */
+  __I  uint32_t  _1_FLTSTAT1;                       /*!< PWM1 Fault Status 1                                                   */
+  __I  uint32_t  RESERVED3[30];
+  __I  uint32_t  _2_FLTSTAT0;                       /*!< PWM2 Fault Status 0                                                   */
+  __I  uint32_t  _2_FLTSTAT1;                       /*!< PWM2 Fault Status 1                                                   */
+  __I  uint32_t  RESERVED4[30];
+  __I  uint32_t  _3_FLTSTAT0;                       /*!< PWM3 Fault Status 0                                                   */
+  __I  uint32_t  _3_FLTSTAT1;                       /*!< PWM3 Fault Status 1                                                   */
+  __I  uint32_t  RESERVED5[397];
+  __IO uint32_t  PP;                                /*!< PWM Peripheral Properties                                             */
+} Custom_PWM0_Type;
+
+void pwm_configure_channel(Custom_PWM0_Type* myPWM, int channel, float frequency, float initial_dc){
+    SystemCoreClockUpdate();
+    int load_value = (int)((SystemCoreClock/(64.0f*frequency)))-1;
+    int gen_block = (channel>>1)&0xf;
+    int is_channel_b = channel&0x1;
+    
+    myPWM->generator[gen_block].CTL = 0;
+    myPWM->generator[gen_block].GENA = (0x3<<6)|(0x2<<0);
+    myPWM->generator[gen_block].GENB = (0x3<<10)|(0x2<<0);
+    myPWM->generator[gen_block].LOAD = load_value;
+
+    
+    if(is_channel_b){
+        myPWM->generator[gen_block].CMPB = (int)((load_value+1) * initial_dc/100);
+    }else{
+        myPWM->generator[gen_block].CMPA = (int)((load_value+1) * initial_dc/100);
+    }
+    
+    myPWM->generator[gen_block].CTL = (0x1<<2)|(0x1<<0);
+    myPWM->ENABLE |= 0x1<<channel;
+}
+
 void pwm0_AB_init(float frequency,int mode,float pwm_a_initial_dc,float pwm_b_initial_dc){
     //Configure PB6 as alternate function for M0PWM0
     if((mode == PWM_A_ONLY)||(mode == PWM_A_AND_B)){
@@ -44,27 +117,23 @@ void pwm0_AB_init(float frequency,int mode,float pwm_a_initial_dc,float pwm_b_in
     SystemCoreClockUpdate();
     int load_value = (int)((SystemCoreClock/(64.0f*frequency)))-1;
 
-    PWM0->_0_CTL = 0;
-    PWM0->_0_GENA = (0x3<<6)|(0x2<<0);
-    PWM0->_0_GENB = (0x3<<10)|(0x2<<0);
-    PWM0->_0_LOAD = load_value;
-    PWM0->_0_CMPA = (int)((load_value+1) * pwm_a_initial_dc/100);
-    PWM0->_0_CMPB = (int)((load_value+1) * pwm_b_initial_dc/100);
-    PWM0->_0_CTL = (0x1<<2)|0x1<<0;
+    Custom_PWM0_Type* myPWM = (Custom_PWM0_Type*)PWM0;
 
-    if(mode == PWM_A_ONLY){
-        PWM0->ENABLE |= 0x1<<0;
-    }else if(mode == PWM_B_ONLY){
-        PWM0->ENABLE |= 0x1<<1;
-    }else{
-        PWM0->ENABLE |= (0x1<<0)|(0x1<<1);
+    if((mode == PWM_A_ONLY)||(mode == PWM_A_AND_B)){
+        pwm_configure_channel(myPWM,0,frequency,pwm_a_initial_dc);
+    }
+    if((mode == PWM_B_ONLY)||(mode == PWM_A_AND_B)){
+        pwm_configure_channel(myPWM,1,frequency,pwm_b_initial_dc);
     }
 }
 
 void pwm0_A_set_dc(float pwm_dc){
-    PWM0->_0_CMPA = (int)((PWM0->_0_LOAD+1)* pwm_dc/100);
+    Custom_PWM0_Type* myPWM = (Custom_PWM0_Type*)PWM0;
+    myPWM->generator[0].CMPA = (int)((myPWM->generator[0].LOAD + 1)* pwm_dc/100);
 }
 
 void pwm0_B_set_dc(float pwm_dc){
-    PWM0->_0_CMPB = (int)((PWM0->_0_LOAD+1)* pwm_dc/100);
+    Custom_PWM0_Type* myPWM = (Custom_PWM0_Type*)PWM0;
+    myPWM->generator[0].CMPB = (int)((myPWM->generator[0].LOAD + 1)* pwm_dc/100);
 }
+
