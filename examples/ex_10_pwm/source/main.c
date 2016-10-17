5d82accce6d72d5930f0582ddd28918703a302da
Manuel Limones <limonesu.me@gmail.com>
2016-10-16 20:53:34 -0500

    Update keil projet and main

--- examples/ex_10_pwm/source/main.c
+++ examples/ex_10_pwm/source/main.c
@@ -1,7 +1,14 @@
 #include "pwm_tm4c.h"
+#include "delay.h"
 
 int main(){
     pwm0_AB_init(500,PWM_A_AND_B,50,30);
     while(1){
+        delay_ms(2000);
+        pwm0_A_set_dc(80);
+        pwm0_B_set_dc(15);
+        delay_ms(2000);
+        pwm0_A_set_dc(50);
+        pwm0_B_set_dc(60);
     }
 }
